90731710c5ab67b5d95cbcc1d4e47891277fc760
Afra Dawood <3frahmalik@gmail.com>
2022-03-08 22:57:32 +0400

    start

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import Nav from './Nav'
+
+function App() {
+  return (
+    <div className="app">
+<Nav />
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Nav.css'
+
+function Nav() {
+  return (
+    <nav className="nav">
+<img className="logo" src="https://colorpedia.org/wp-content/uploads/2021/09/airbnb-logo-300x225.png" alt="logo" />
+
+    </nav>
+  )
+}
+
+export default Nav
